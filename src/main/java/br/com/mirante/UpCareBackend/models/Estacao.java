b41898e25ae7d59f5e9c2accc889f3b9a4558621
Martins98725 <icaro25martins@gmail.com>
2024-10-31 11:22:19 -0300

    fix: colocando anotação collumn na entidade de estação

--- src/main/java/br/com/mirante/UpCareBackend/models/Estacao.java
+++ src/main/java/br/com/mirante/UpCareBackend/models/Estacao.java
@@ -1,6 +1,7 @@
 package br.com.mirante.UpCareBackend.models;
 
 import br.com.mirante.UpCareBackend.models.enums.Status;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
@@ -17,15 +18,21 @@ public class Estacao {
     @Id
     private UUID id;
 
+    @Column(name = "NOME", nullable = false, length = 50)
     private String nome;
 
+    @Column(name = "LONGITUDE", nullable = false, length = 100)
     private double longitude;
 
+    @Column(name = "LATITUDE", nullable = false, length = 100)
     private double latitude;
 
+    @Column(name = "ENDERECO", nullable = false, length = 50)
     private String endereco;
 
+    @Column(name = "LINK_GRAFANA", nullable = false, length = 75)
     private String linkGrafana;
 
+    @Column(name = "STATUS")
     private Status status;
 }
