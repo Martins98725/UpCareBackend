0e9111017c1b74783951ecb9e9696426ebf6d805
Martins98725 <icaro25martins@gmail.com>
2024-10-31 11:24:00 -0300

    feat: terminando dto de estação

--- src/main/java/br/com/mirante/UpCareBackend/dto/EstacaoDTO.java
+++ src/main/java/br/com/mirante/UpCareBackend/dto/EstacaoDTO.java
@@ -1,4 +1,22 @@
 package br.com.mirante.UpCareBackend.dto;
 
+import br.com.mirante.UpCareBackend.models.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EstacaoDTO {
+    private UUID id;
+    private String nome;
+    private double longitude;
+    private double latitude;
+    private String endereco;
+    private String linkGrafana;
+    private Status status;
+
 }
