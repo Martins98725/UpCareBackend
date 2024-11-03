601778fef0d2119ab8c9c60ecc8a3ac45cf33f08
Martins98725 <icaro25martins@gmail.com>
2024-11-03 18:40:01 -0300

    feat: criando mapper de antena

---
+++ src/main/java/br/com/mirante/UpCareBackend/mappers/AntenaMapper.java
@@ -0,0 +1,15 @@
+package br.com.mirante.UpCareBackend.mappers;
+
+import br.com.mirante.UpCareBackend.dto.AntenaDTO;
+import br.com.mirante.UpCareBackend.models.Antena;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AntenaMapper {
+    AntenaMapper INTANCE = Mappers.getMapper(AntenaMapper.class);
+
+    AntenaDTO antenaToAntenaDTO(Antena antena);
+
+    Antena antenaDTOToAntena(AntenaDTO antenaDTO);
+}
