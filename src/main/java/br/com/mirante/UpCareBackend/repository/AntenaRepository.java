650e2cd57dee273773c57e2f9e6af9bd25650731
Martins98725 <icaro25martins@gmail.com>
2024-11-03 18:32:40 -0300

    feat: criando repository

---
+++ src/main/java/br/com/mirante/UpCareBackend/repository/AntenaRepository.java
@@ -0,0 +1,12 @@
+package br.com.mirante.UpCareBackend.repository;
+
+import br.com.mirante.UpCareBackend.models.Antena;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AntenaRepository extends JpaRepository<Antena, UUID>, JpaSpecificationExecutor<Antena> {
+}
