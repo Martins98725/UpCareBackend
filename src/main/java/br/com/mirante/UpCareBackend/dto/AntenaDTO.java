03d1ac508d2fafe7e70ab0dfc63a311b1e264d7e
Martins98725 <icaro25martins@gmail.com>
2024-11-03 18:46:52 -0300

    fix: ajustando dto de antena

--- src/main/java/br/com/mirante/UpCareBackend/dto/AntenaDTO.java
+++ src/main/java/br/com/mirante/UpCareBackend/dto/AntenaDTO.java
@@ -1,11 +1,29 @@
 package br.com.mirante.UpCareBackend.dto;
 
+import br.com.mirante.UpCareBackend.models.Estacao;
+import br.com.mirante.UpCareBackend.models.enums.Categoria;
+import br.com.mirante.UpCareBackend.models.enums.PosicaoDaTorre;
+import br.com.mirante.UpCareBackend.models.enums.Status;
+import br.com.mirante.UpCareBackend.models.enums.TipoAntena;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class AntenaDTO {
+    private UUID id;
+    private String codigo;
+    private String marca;
+    private String modelo;
+    private Status status;
+    private double vr;
+    private TipoAntena tipoAntena;
+    private String gain;
+    private PosicaoDaTorre posicaoDaTorre;
+    private Categoria categoria;
+    private EstacaoDTO estacao;
 }
