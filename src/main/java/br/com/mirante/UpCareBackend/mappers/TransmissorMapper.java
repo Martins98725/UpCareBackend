f605838037eff1d7f58b4f1b7c7ff11451046b8f
Martins98725 <icaro25martins@gmail.com>
2024-11-07 10:50:08 -0300

    feat: terminando transmissor service e transmissor mapper

--- src/main/java/br/com/mirante/UpCareBackend/mappers/TransmissorMapper.java
+++ src/main/java/br/com/mirante/UpCareBackend/mappers/TransmissorMapper.java
@@ -1,7 +1,15 @@
 package br.com.mirante.UpCareBackend.mappers;
 
+import br.com.mirante.UpCareBackend.dto.TrasnmissorDTO;
+import br.com.mirante.UpCareBackend.models.Transmissor;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface TransmissorMapper {
+    TransmissorMapper INSTANCE = Mappers.getMapper(TransmissorMapper.class);
+
+    TrasnmissorDTO transmissorToTransmissorDTO(Transmissor transmissor);
+
+    Transmissor transmissorDTOToTransmissor(TrasnmissorDTO trasnmissorDTO);
 }
--- src/main/java/br/com/mirante/UpCareBackend/service/Impl/TransmissorServiceImpl.java
+++ src/main/java/br/com/mirante/UpCareBackend/service/Impl/TransmissorServiceImpl.java
@@ -2,7 +2,9 @@ package br.com.mirante.UpCareBackend.service.Impl;
 
 import br.com.mirante.UpCareBackend.dto.TrasnmissorDTO;
 import br.com.mirante.UpCareBackend.exceptions.BusinessException;
+import br.com.mirante.UpCareBackend.mappers.TransmissorMapper;
 import br.com.mirante.UpCareBackend.models.Transmissor;
+import br.com.mirante.UpCareBackend.repository.TransmissorRepository;
 import br.com.mirante.UpCareBackend.service.TrasnmissorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -15,28 +17,44 @@ import java.util.UUID;
 
 @Service
 public class TransmissorServiceImpl implements TrasnmissorService {
+    @Autowired
+    private TransmissorRepository transmissorRepository;
+
     @Override
     public Page<Transmissor> findAll(Pageable page, Specification<Transmissor> specification) {
-        return null;
+        return transmissorRepository.findAll(specification, page);
     }
 
     @Override
     public TrasnmissorDTO save(TrasnmissorDTO trasnmissorDTO) throws BusinessException {
-        return null;
+        Transmissor transmissor = TransmissorMapper.INSTANCE.transmissorDTOToTransmissor(trasnmissorDTO);
+
+        transmissor = transmissorRepository.save(transmissor);
+        return TransmissorMapper.INSTANCE.transmissorToTransmissorDTO(transmissor);
     }
 
     @Override
     public Optional<TrasnmissorDTO> findById(UUID id) {
-        return Optional.empty();
+        return transmissorRepository.findById(id).map(TransmissorMapper.INSTANCE::transmissorToTransmissorDTO);
     }
 
     @Override
     public Optional<TrasnmissorDTO> update(UUID id, TrasnmissorDTO trasnmissorDTO) {
+        if (transmissorRepository.existsById(id)){
+            Transmissor transmissor = TransmissorMapper.INSTANCE.transmissorDTOToTransmissor(trasnmissorDTO);
+            transmissor.setId(id);
+            transmissor = transmissorRepository.save(transmissor);
+            return Optional.of(TransmissorMapper.INSTANCE.transmissorToTransmissorDTO(transmissor));
+        }
         return Optional.empty();
     }
 
     @Override
     public boolean delete(UUID id) {
+        if (transmissorRepository.existsById(id)) {
+            transmissorRepository.deleteById(id);
+            return true;
+        }
         return false;
     }
 }
