b4cd8c4c39fc93a3fe76029012924e6bc298e0e9
Martins98725 <icaro25martins@gmail.com>
2024-11-07 10:52:12 -0300

    feat: terminando transmissor dto

--- src/main/java/br/com/mirante/UpCareBackend/dto/TrasnmissorDTO.java
+++ src/main/java/br/com/mirante/UpCareBackend/dto/TrasnmissorDTO.java
@@ -1,11 +1,26 @@
 package br.com.mirante.UpCareBackend.dto;
 
+import br.com.mirante.UpCareBackend.models.enums.Categoria;
+import br.com.mirante.UpCareBackend.models.enums.Status;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class TrasnmissorDTO {
+    private UUID id;
+    private String codigo;
+    private String marca;
+    private String modelo;
+    private Categoria categoria;
+    private Status status;
+    private float programado;
+    private float canalVIrtual;
+    private float canalFisico;
+    private String acopladorUm;
+    private String acopladorDois;
 }
