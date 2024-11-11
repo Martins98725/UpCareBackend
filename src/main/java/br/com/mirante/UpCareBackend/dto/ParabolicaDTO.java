f47000481f486b3011601072bc540830d3de2c3d
Martins98725 <icaro25martins@gmail.com>
2024-11-11 10:49:09 -0300

    feat: terminando dto de parabolica

--- src/main/java/br/com/mirante/UpCareBackend/dto/ParabolicaDTO.java
+++ src/main/java/br/com/mirante/UpCareBackend/dto/ParabolicaDTO.java
@@ -1,11 +1,26 @@
 package br.com.mirante.UpCareBackend.dto;
 
+import br.com.mirante.UpCareBackend.models.Estacao;
+import br.com.mirante.UpCareBackend.models.enums.Categoria;
+import br.com.mirante.UpCareBackend.models.enums.Status;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ParabolicaDTO {
+    private UUID id;
+    private String codigo;
+    private String marca;
+    private String modelo;
+    private Categoria categoria;
+    private Status status;
+    private float diametro;
+    private String satelite;
+    //private Estacao estacao;
+
 }
