9f87eec596c9a0d36ec038ff6bb792e582248b89
Martins98725 <icaro25martins@gmail.com>
2024-11-11 09:27:16 -0300

    feat: terminando mapper de parabolica

--- src/main/java/br/com/mirante/UpCareBackend/mappers/ParabolicaMapper.java
+++ src/main/java/br/com/mirante/UpCareBackend/mappers/ParabolicaMapper.java
@@ -1,7 +1,15 @@
 package br.com.mirante.UpCareBackend.mappers;
 
+import br.com.mirante.UpCareBackend.dto.ParabolicaDTO;
+import br.com.mirante.UpCareBackend.models.Parabolica;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface ParabolicaMapper {
+    ParabolicaMapper INSTANCE = Mappers.getMapper(ParabolicaMapper.class);
+
+    ParabolicaDTO parabolicaToParabolicaDTO(Parabolica parabolica);
+
+    Parabolica parabolicaDTOToParabolica(ParabolicaDTO parabolicaDTO);
 }
