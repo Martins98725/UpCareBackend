4ce70ff0d4623091bda99f45468eeddeaccbfc1f
Martins98725 <icaro25martins@gmail.com>
2024-11-13 09:20:55 -0300

    feat: adicionando mapper de combinador

---
+++ src/main/java/br/com/mirante/UpCareBackend/mappers/CombinadorMapper.java
@@ -0,0 +1,15 @@
+package br.com.mirante.UpCareBackend.mappers;
+
+import br.com.mirante.UpCareBackend.dto.CombinadorDTO;
+import br.com.mirante.UpCareBackend.models.Combinador;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CombinadorMapper {
+    CombinadorMapper INSTANCE = Mappers.getMapper(CombinadorMapper.class);
+
+    CombinadorDTO combinadorToCombinadorDTO(Combinador combinador);
+
+    Combinador combinadorDTOToCombinador(CombinadorDTO);
+}
