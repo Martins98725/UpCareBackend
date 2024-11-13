5dda5109ae168d0dac0b991fa2a2c74a6fd3a789
Martins98725 <icaro25martins@gmail.com>
2024-11-13 09:18:08 -0300

    feat: adicionando dto de combinador

---
+++ src/main/java/br/com/mirante/UpCareBackend/dto/CombinadorDTO.java
@@ -0,0 +1,21 @@
+package br.com.mirante.UpCareBackend.dto;
+
+import br.com.mirante.UpCareBackend.models.enums.Categoria;
+import br.com.mirante.UpCareBackend.models.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CombinadorDTO {
+    private UUID id;
+    private String codigo;
+    private String marca;
+    private String modelo;
+    private Categoria categoria;
+    private Status status;
+}
